dfd9a214d2b26b3a6ada6f5e39843d19c485365d
Dan <danblinski@yahoo.com>
2017-03-03 23:12:52 -0600

    i

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -132,8 +132,8 @@ syscall(void)
 
   num = proc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    proc->tf->eax = syscalls[num]();
     proc->count[num] = proc->count[num] + 1;
+    proc->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             proc->pid, proc->name, num);
--- xv6-public/test.c
+++ xv6-public/test.c
@@ -3,8 +3,16 @@
 #include "user.h"
 
 int main(void){
-  printf(1, "firstname_lastname_studentid");
-  printf(1, "getting calls");
+  //printf(1, "firstname_lastname_studentid");
+  printf(1, "Initial calls\n");
   totalcalls();
+  if(fork() == 0){
+    printf(1, "testing fork");
+  }
+  else{
+    wait();
+    printf(1, "wait");
+  }
+  printf(1, "Final calls\n");
   exit();
 }
