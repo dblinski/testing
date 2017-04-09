c7e2f8e02896bea2199974187d467cdc8ceb7f10
Dan -Virtualbox <dszablin@hawk.iit.edu>
2017-04-08 21:28:07 -0500

    3b

--- xv6-3/sysproc.c
+++ xv6-3/sysproc.c
@@ -96,5 +96,21 @@ int
 sys_memusage(void)
 {
   int countP = 0, countW = 0, countU = 0;
-  
+  uint i = 0;
+  for(i = 0; i < proc->sz; i += PGSIZE){
+      if(proc->pgdir[i] & PTE_P){
+          countP++;
+      }
+      if(proc->pgdir[i] & PTE_W){
+          countW++;
+      }
+      if(proc->pgdir[i] & PTE_U){
+          countU++;
+      }
+  }
+  cprintf("Number of pages: %d = %d bytes\n", countP, (countP * PGSIZE));
+  cprintf("Number of writable pages: %d\n", countW);
+  cprintf("Number of accessible pages: %d\n", countU);
+
+  return 1;
 }
--- xv6-3/test.c
+++ xv6-3/test.c
@@ -3,5 +3,11 @@
 #include "user.h"
 
 int main(void){
-  
+  printf(1, "testing system call\n");
+  memusage();
+  void *i = malloc(4096);
+  printf(1, "testing again\n");
+  memusage();
+  free(i);
+  exit();
 }
